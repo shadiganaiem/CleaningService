1ebf3bbf8cdbcd629c6737eb2d52759498def9a6
Shady <Shady@ADMINRG-6797DQV>
2019-11-12 15:23:24 +0200

    Initial commit

---
+++ app/src/main/java/com/cleaningservice/cleaningservice/MainActivity.java
@@ -0,0 +1,27 @@
+package com.cleaningservice.cleaningservice;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private Button SignUpButton;
+    private Button SignInButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        SignUpButton= findViewById(R.id.SignUpBtn);
+        SignInButton= findViewById(R.id.SignInBtn);
+    }
+
+    public void OpenRegisterActivity(View v){
+        Intent intent = new Intent(this,RegisterActivity.class);
+        startActivity(intent);
+    }
+}
