1d11529b5f99690bc9cddb0be3489c216cb13bcd
Shady <Shady@ADMINRG-6797DQV>
2019-11-12 17:00:37 +0200

    changes

--- app/src/main/java/com/cleaningservice/cleaningservice/RegisterActivity.java
+++ app/src/main/java/com/cleaningservice/cleaningservice/RegisterActivity.java
@@ -1,14 +1,31 @@
 package com.cleaningservice.cleaningservice;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class RegisterActivity extends AppCompatActivity {
+    private ConstraintLayout FirstStep;
+    private ConstraintLayout SecondStep;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        FirstStep = findViewById(R.id.FirstStep);
+        SecondStep = findViewById(R.id.SecondStep);
+
+        FirstStep.setVisibility(View.VISIBLE);
+        SecondStep.setVisibility(View.INVISIBLE);
+    }
+
+    public void NextStepRegister(View v){
+        FirstStep.setVisibility(View.INVISIBLE);
+        SecondStep.setVisibility(View.VISIBLE);
     }
 }
